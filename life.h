3a747f56035f5bb3b8b28e16607a43a1046e69f1
Hubert Nakielski <hubert.nakielski@gmail.com>
2020-02-28 14:48:53 +0100

    Added a skeleton

--- life.h
+++ life.h
@@ -7,4 +7,11 @@ typedef struct {
     char **data;
 } life, *plife;
 
+plife generate_life(plife previous);
+
+plife load_life(char *file_name);
+int save_life(char *file_name, plife life);
+
+int save_life_png(char *file_name, plife life);
+
 #endif
